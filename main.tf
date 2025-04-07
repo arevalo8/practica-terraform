51fecc05d1519aac2f3d4d4cfcd73cd841eb4e3d
arevalo8 <110304077+arevalo8@users.noreply.github.com>
2025-04-07 16:29:42 +0200

    update main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -10,57 +10,67 @@ terraform {
 provider "docker" {}
 
 # Red Docker
-resource "docker_network" "wp_network" {
-  name = "wp_network"
+resource "docker_network" "wordpress_net" {
+  name = "wordpress_network"
 }
 
-# Volumen para la base de datos
-resource "docker_volume" "db_volume" {
-  name = "db_volume"
+# Volumen para MariaDB
+resource "docker_volume" "mariadb_data" {
+  name = "mariadb_data"
 }
 
 # Contenedor MariaDB
+resource "docker_image" "mariadb" {
+  name         = "mariadb:latest"
+  keep_locally = false
+}
+
 resource "docker_container" "mariadb" {
-  image = "mariadb:latest"
   name  = "mariadb_container"
-  restart = "always"
+  image = docker_image.mariadb.image_id
 
   env = [
-    "MYSQL_ROOT_PASSWORD=${var.db_root_password}",
-    "MYSQL_DATABASE=${var.db_name}",
-    "MYSQL_USER=${var.db_user}",
-    "MYSQL_PASSWORD=${var.db_password}"
+    "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}",
+    "MYSQL_DATABASE=${var.mysql_database}",
+    "MYSQL_USER=${var.mysql_user}",
+    "MYSQL_PASSWORD=${var.mysql_password}"
   ]
 
   volumes {
-    volume_name    = docker_volume.db_volume.name
+    volume_name    = docker_volume.mariadb_data.name
     container_path = "/var/lib/mysql"
   }
 
   networks_advanced {
-    name = docker_network.wp_network.name
+    name = docker_network.wordpress_net.name
   }
 }
 
 # Contenedor WordPress
-resource "docker_container" "wordpress" {
-  image = "wordpress:latest"
-  name  = "wordpress_container"
-  restart = "always"
+resource "docker_image" "wordpress" {
+  name         = "wordpress:latest"
+  keep_locally = false
+}
 
-  env = [
-    "WORDPRESS_DB_HOST=mariadb_container",
-    "WORDPRESS_DB_NAME=${var.db_name}",
-    "WORDPRESS_DB_USER=${var.db_user}",
-    "WORDPRESS_DB_PASSWORD=${var.db_password}"
-  ]
+resource "docker_container" "wordpress" {
+  name  = "wordpress_container"
+  image = docker_image.wordpress.image_id
 
   ports {
     internal = 80
     external = 8080
   }
 
+  env = [
+    "WORDPRESS_DB_HOST=${var.wordpress_db_host}",
+    "WORDPRESS_DB_NAME=${var.mysql_database}",
+    "WORDPRESS_DB_USER=${var.mysql_user}",
+    "WORDPRESS_DB_PASSWORD=${var.mysql_password}"
+  ]
+
   networks_advanced {
-    name = docker_network.wp_network.name
+    name = docker_network.wordpress_net.name
   }
+
+  depends_on = [docker_container.mariadb]
 }
--- variables.tf
+++ variables.tf
@@ -1,47 +1,29 @@
-variable "network_name" {
-  description = "Nombre de la red Docker"
+variable "mysql_root_password" {
+  description = "Root password for MariaDB"
   type        = string
-  default     = "wordpress_network"
+  default     = "rootpass"
 }
 
-variable "db_container_name" {
-  description = "Nombre del contenedor de la base de datos"
-  type        = string
-  default     = "mariadb_container"
-}
-
-variable "db_root_password" {
-  description = "Contraseña de root de la base de datos"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_name" {
-  description = "Nombre de la base de datos"
+variable "mysql_database" {
+  description = "Database name for WordPress"
   type        = string
   default     = "wordpress_db"
 }
 
-variable "db_user" {
-  description = "Usuario de la base de datos"
+variable "mysql_user" {
+  description = "Database user for WordPress"
   type        = string
-  default     = "wordpress_user"
+  default     = "wp_user"
 }
 
-variable "db_password" {
-  description = "Contraseña del usuario de la base de datos"
+variable "mysql_password" {
+  description = "Database user password"
   type        = string
-  sensitive   = true
+  default     = "wp_pass"
 }
 
-variable "wordpress_container_name" {
-  description = "Nombre del contenedor de WordPress"
+variable "wordpress_db_host" {
+  description = "Hostname of the database for WordPress"
   type        = string
-  default     = "wordpress_container"
-}
-
-variable "wordpress_port" {
-  description = "Puerto en el que se ejecutará WordPress"
-  type        = number
-  default     = 8080
+  default     = "mariadb_container:3306"
 }
